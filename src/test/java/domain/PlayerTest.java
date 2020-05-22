4a4c2790b611e562b449c15003593f69283a620f
kimminsub <msmk530@naver.com>
2020-05-22 15:56:40 +0900

    refactor : 검증 테스트는 ValidatorTest에서 실행

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -22,19 +22,13 @@ class PlayerTest {
     }
 
     @Test
-    void 이름_최대길이_예외발생_테스트() {
-        assertThatExceptionOfType(LengthOfNameException.class)
-                .isThrownBy(() -> new Player("kimminsub"));
-    }
-
-    @Test
     void 플레이어_당첨번호_매칭_테스트() {
-        Map<String, Integer> gameResult= new HashMap<>();
+        Map<String, Integer> gameResult = new HashMap<>();
         List<Integer> matchedItems = new ArrayList<>();
 
         matchedItems.add(500);
 
-        Map<String, Integer> result = player.matchItemNumbers(gameResult,matchedItems,0);
+        Map<String, Integer> result = player.matchItemNumbers(gameResult, matchedItems, 0);
 
         assertThat(result.get("kim")).isEqualTo(500);
     }
